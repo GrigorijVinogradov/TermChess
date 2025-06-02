8ae5e09f320997fed5414d943be2d7ca64a465ef
xgvx <grischa.vinogradov@gmail.com>
2025-06-02 23:06:25 +0200

    intial board

--- board.py
+++ board.py
@@ -1,7 +1,41 @@
 import pieces
 
 size = 8
-board_map = [['' for _ in range (size)] for _ in range (size)] 
+board_map = [['' for _ in range (size)] for _ in range (size)]
+
+def initialize_board():
+    initialize_pawns()
+    initialize_rooks()
+    initialize_knights()
+    initialize_bishops()
+    initialize_royals()
+
+def initialize_symmetrical_pieces(row, column, piece):
+    board_map[row][size-column-1] = piece
+    board_map[row][column] = piece
+
+def initialize_pawns():
+    for column, _ in enumerate(board_map):
+        board_map[1][column] = pieces.black_pawn 
+        board_map[6][column] = pieces.white_pawn
+
+def initialize_rooks():
+    initialize_symmetrical_pieces(0, 0, pieces.black_rook)
+    initialize_symmetrical_pieces(7, 0, pieces.white_rook)
+
+def initialize_knights():
+    initialize_symmetrical_pieces(0, 1, pieces.black_knight)
+    initialize_symmetrical_pieces(7, 1, pieces.white_knight)
+
+def initialize_bishops():
+    initialize_symmetrical_pieces(0, 2, pieces.black_bishop)
+    initialize_symmetrical_pieces(7, 2, pieces.white_bishop)
+
+def initialize_royals():
+    board_map[0][3] = pieces.black_queen
+    board_map[0][4] = pieces.black_king
+    board_map[7][3] = pieces.white_queen
+    board_map[7][4] = pieces.white_king
 
 def is_even(number):
     return number % 2 == 0
@@ -12,6 +46,9 @@ def decide_square(row, column):
     is_row_even = is_even(row)
     return pieces.black_square if is_even(column) == is_row_even else pieces.white_square
 
+# Main ☆*: .｡. o(≧▽≦)o .｡.:*☆
+
+initialize_board()
 for row, i in enumerate(board_map):
     print(size-row, end=" ")
     for column, j in enumerate(i):
--- pieces.py
+++ pieces.py
@@ -1,2 +1,26 @@
-black_square = '◼'
-white_square = '◻'
+from termcolor import colored
+
+def black(character):
+    return colored(character, 'red') 
+
+white_square = '◼'
+black_square = black(white_square)
+
+white_pawn = '♙'
+black_pawn = black(white_pawn)
+
+white_knight = '♘'
+black_knight = black(white_knight)
+
+white_bishop = '♗'
+black_bishop = black(white_bishop)
+
+white_rook = '♖'
+black_rook = black(white_rook)
+
+white_queen = '♕'
+black_queen = black(white_queen)
+
+white_king = '♔'
+black_king = black(white_king)
+
