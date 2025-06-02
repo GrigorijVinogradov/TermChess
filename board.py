0eace29e2298d5dc267dec0e534d2387dc59d99b
xgvx <grischa.vinogradov@gmail.com>
2025-06-02 23:20:01 +0200

    board generator file

--- board.py
+++ board.py
@@ -1,41 +1,8 @@
 import pieces
+import board_generator
 
 size = 8
-board_map = [['' for _ in range (size)] for _ in range (size)]
-
-def initialize_board():
-    initialize_pawns()
-    initialize_rooks()
-    initialize_knights()
-    initialize_bishops()
-    initialize_royals()
-
-def initialize_symmetrical_pieces(row, column, piece):
-    board_map[row][size-column-1] = piece
-    board_map[row][column] = piece
-
-def initialize_pawns():
-    for column, _ in enumerate(board_map):
-        board_map[1][column] = pieces.black_pawn 
-        board_map[6][column] = pieces.white_pawn
-
-def initialize_rooks():
-    initialize_symmetrical_pieces(0, 0, pieces.black_rook)
-    initialize_symmetrical_pieces(7, 0, pieces.white_rook)
-
-def initialize_knights():
-    initialize_symmetrical_pieces(0, 1, pieces.black_knight)
-    initialize_symmetrical_pieces(7, 1, pieces.white_knight)
-
-def initialize_bishops():
-    initialize_symmetrical_pieces(0, 2, pieces.black_bishop)
-    initialize_symmetrical_pieces(7, 2, pieces.white_bishop)
-
-def initialize_royals():
-    board_map[0][3] = pieces.black_queen
-    board_map[0][4] = pieces.black_king
-    board_map[7][3] = pieces.white_queen
-    board_map[7][4] = pieces.white_king
+board_map = board_generator.get_default_board()
 
 def is_even(number):
     return number % 2 == 0
@@ -48,7 +15,6 @@ def decide_square(row, column):
 
 # Main ☆*: .｡. o(≧▽≦)o .｡.:*☆
 
-initialize_board()
 for row, i in enumerate(board_map):
     print(size-row, end=" ")
     for column, j in enumerate(i):
