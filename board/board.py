08b458d6f4c8b54da3ba9e3db06d46f8241d43dc
g.vinogradov <grigorij.vinogradovx@gmail.com>
2026-06-14 17:29:46 +0200

    use coordinate object in other board methods

--- board/board.py
+++ board/board.py
@@ -11,13 +11,13 @@ class Board:
             for column_id, _ in enumerate(row):
                 self.board_map[row_id][column_id] = ''
 
-    def move_piece(self, fromD, fromL, toD, toL):
-        from_piece = self.get_piece(fromD, fromL)
-        self.set_piece(toD, toL, from_piece)
-        self.set_piece(fromD, fromL, '')
+    def move_piece(self, from_coords: Coordinates, to_coords: Coordinates):
+        from_piece = self.get_piece(from_coords)
+        self.set_piece(to_coords, from_piece)
+        self.set_piece(from_coords, '')
 
-    def get_piece(self, cordL, cordD) -> Piece:
-        return self.board_map[cordL][cordD]
+    def get_piece(self, coordinates: Coordinates) -> Piece:
+        return self.board_map[coordinates.l][coordinates.d]
 
     def set_piece(self, coordinates: Coordinates, piece):
         self.board_map[coordinates.l][coordinates.d] = piece 
