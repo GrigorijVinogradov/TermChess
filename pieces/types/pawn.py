dcaf41fe1dd61af5457c6cceaa888f4cbb688a45
g.vinogradov <grigorij.vinogradovx@gmail.com>
2026-06-26 14:56:41 +0200

    straight collision check for pawn when moving 2 fields

--- pieces/types/pawn.py
+++ pieces/types/pawn.py
@@ -1,5 +1,6 @@
 from board.coordinates import Coordinates
 from pieces.collision_checker import check_collision, get_direct_colliding_piece
+from pieces.patterns.straight_checker import get_straight_colliding_piece
 from pieces.piece import Piece
 from pieces.enums.piece_icons import Piece_Icons
 from pieces.enums.colors import Color
@@ -19,6 +20,7 @@ class Pawn(Piece):
 
         if to_coord.d == from_coord.d - 2*direction and self.step_count == 0:
             self.step_count += 1 
+            check_collision(get_straight_colliding_piece, from_coord, to_coord, self.color)
             return
 
         if to_coord.d != from_coord.d - 1*direction:
